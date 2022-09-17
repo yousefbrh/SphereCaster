9827864faf5593706eb14dd31a02524f5f89f6bd
Yousef Borhani <yousef@joyixir.com>
2022-09-18 00:19:51 +0430

    Add sphere cast

--- Assets/Scripts/SphereCaster.cs
+++ Assets/Scripts/SphereCaster.cs
@@ -6,10 +6,12 @@ public class SphereCaster : MonoBehaviour
     public float sphereRadius;
     public float maxDistance;
     public LayerMask layerMask;
+    
+    // Sphere Cast Export
+    public GameObject currentHitObject;
 
     // Sphere Cast All Export
     public List<GameObject> currentHitObjects = new List<GameObject>();
-
 
     private Vector3 _origin;
     private Vector3 _direction;
@@ -18,21 +20,29 @@ public class SphereCaster : MonoBehaviour
 
     private void Update()
     {
+        _origin = transform.position;
+        _direction = transform.forward;
+        
         SphereCastAll();
     }
 
-    private void OnDrawGizmosSelected()
+    private void SphereCast()
     {
-        Gizmos.color = Color.red;
-        Debug.DrawLine(_origin, _origin + _direction * _currentHitDistance);
-        Gizmos.DrawWireSphere(_origin + _direction * _currentHitDistance, sphereRadius);
+        RaycastHit raycastHit;
+        if (Physics.SphereCast(_origin, sphereRadius, _direction, out raycastHit, maxDistance, layerMask, QueryTriggerInteraction.UseGlobal))
+        {
+            currentHitObject = raycastHit.transform.gameObject;
+            _currentHitDistance = raycastHit.distance;
+        }
+        else
+        {
+            currentHitObject = null;
+            _currentHitDistance = maxDistance;
+        }
     }
-
+    
     private void SphereCastAll()
     {
-        _origin = transform.position;
-        _direction = transform.forward;
-
         _currentHitDistance = maxDistance;
         currentHitObjects.Clear();
 
@@ -43,6 +53,13 @@ public class SphereCaster : MonoBehaviour
             currentHitObjects.Add(hit.transform.gameObject);
             _currentHitDistance = hit.distance;
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Debug.DrawLine(_origin, _origin + _direction * _currentHitDistance);
+        Gizmos.DrawWireSphere(_origin + _direction * _currentHitDistance, sphereRadius);
     }
 
 }
